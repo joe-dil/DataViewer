f8cc4fa8bbbc534c17e5be5c21130fc292bc5fb7
Joe Dillon <joe.dillon.dev@proton.me>
2025-07-06 04:09:32 -0400

    Change default header color from yellow to green
    - Update color pair 1 to use COLOR_GREEN instead of COLOR_YELLOW
    - Headers now display in terminal green on black background
    - Maintains underline styling for headers

--- src/main.c
+++ src/main.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]) {
 #ifndef TEST_BUILD
     initscr();
     start_color();
-    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
